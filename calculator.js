00151463aa1cbd0708902d003a776e555655ee8c
Akinbola Kehinde <112096641+Akinbola247@users.noreply.github.com>
2022-09-14 13:28:11 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 let display = document.querySelector('.calculatordisplay');
+let display2 = document.querySelector('.calculatordisplayleft')
 let buttons = Array.from(document.querySelectorAll('button'));
 
 buttons.map( buttons => {
@@ -7,21 +8,22 @@ buttons.map( buttons => {
         switch (e.target.innerText) {
             case 'AC':
                 display.innerText = " ";
+                display2.innerText = " ";
             break;
             case '←':
                 if (display.innerText){
                     display.innerText = display.innerText.slice(0, -1);
+                    display2.innerText = display.innerText.slice(0,-1);
                 }    
             break;
             case "=":
+                display2.innerText= display.innerText;
                 try {
                     display.innerText = eval(display.innerText);
                 } catch {
                     display.innerText = 'ERROR!'
                 }
-                
             break;
-
                 default: display.innerText += e.target.innerText;   
                
         }
